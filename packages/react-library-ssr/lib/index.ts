7a08602e977eccdec0ba8489119522d003d89f8b
GTA <gregory.taudin@exfabrica.io>
2024-11-26 09:17:18 +0100

    Fix

--- packages/react-library-ssr/lib/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './components/stencil-generated/components';
-export { setAssetPath } from "@test/stencil-components/dist/components/index";
--- packages/react-library/lib/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './components/stencil-generated/components';
-export { setAssetPath } from "@test/stencil-components/dist/components/index";
-//import { setAssetPath } from "@test/stencil-components/dist/components/index";
-//setAssetPath(`${window.location.origin}`);
